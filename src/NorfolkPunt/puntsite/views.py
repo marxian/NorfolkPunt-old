afecc46f0b437baec544ca19e33cabcd78985ee5
Rupert Redington <rupert@neontribe.co.uk>
2012-05-26 12:48:12 +0100

    Moving forward with the front page

--- src/NorfolkPunt/puntsite/views.py
+++ src/NorfolkPunt/puntsite/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from localboats.models import Boat, Picture
+from localboats.events.models import Event
 
 def rules(request):
     return render_to_response('puntsite/rules.html', 
@@ -12,6 +14,14 @@ def history(request):
                               context_instance=RequestContext(request))
     
 def home(request):
+    all_pics = Picture.objects.order_by('?')[:5]
+    landscapes = [x for x in all_pics if x.get_display_size()[0] > x.get_display_size()[1]]
+    
+    next_event = Event.objects.all()[0]
+    
     return render_to_response('puntsite/home.html',
-                              {},
+                              {
+                                "slides": landscapes,
+                                "event": next_event
+                              },
                               context_instance=RequestContext(request))
