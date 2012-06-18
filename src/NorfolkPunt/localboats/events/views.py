7ed1732e32e83750e31f4ce959ad0794614f4712
Rupert Redington <rupert@neontribe.co.uk>
2012-06-18 08:06:47 -0700

    Better event listings
    * Marginally smaller background image

--- src/NorfolkPunt/localboats/events/views.py
+++ src/NorfolkPunt/localboats/events/views.py
@@ -1,13 +1,17 @@
 from models import Event, EventType, RaceResult
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from datetime import datetime
 
 def events(request):
     events = Event.objects.all()
     event_types = EventType.objects.all()
-    
+    past = events.filter(start__lte=datetime.now()).order_by('-start')
+    future = events.filter(start__gte=datetime.now()).order_by('start')
     return render_to_response('localboats/events/events.html', 
                               {'events':events,
+                               'past': past,
+                               'future': future,
                                'event_types':event_types},
                                context_instance=RequestContext(request))
     
