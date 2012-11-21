576925221209e198058463b01b8eccadcef7c77a
Rupert Redington <rupert@neontribe.co.uk>
2012-11-21 22:03:25 +0000

    Styling tweaks and front page forsale

--- src/NorfolkPunt/puntsite/views.py
+++ src/NorfolkPunt/puntsite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from localboats.models import Boat, Picture
+from localboats.sale.models import Sale
 from localboats.events.models import Event
 import datetime
 
@@ -17,14 +18,19 @@ def history(request):
 def home(request):
     all_pics = Picture.objects.order_by('?')[:10]
     landscapes = [x for x in all_pics if x.get_display_size()[0] > x.get_display_size()[1]]
+    
     today = datetime.date.today()
     next_events = Event.objects.filter(
         start__gte=today, # Starting next
     ).order_by('start')
     next_event = len(next_events) and next_events[0] or False
+    
+    forsale = Sale.objects.all()
+
     return render_to_response('puntsite/home.html',
                               {
                                 "slides": landscapes,
-                                "event": next_event
+                                "event": next_event,
+                                "sales": forsale
                               },
                               context_instance=RequestContext(request))
