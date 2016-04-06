d9ac9df47266f549c3292e554beeb8e6d10551a3
Rupert Redington <rupert@neontribe.co.uk>
2016-04-06 12:47:31 +0100

    Travellers filter

--- src/NorfolkPunt/localboats/events/models.py
+++ src/NorfolkPunt/localboats/events/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 from django.template.defaultfilters import slugify, truncatewords
 from datetime import datetime
-from django.contrib.humanize.templatetags.humanize import ordinal 
+from django.contrib.humanize.templatetags.humanize import ordinal
 
 from localboats.models import slugify_uniquely, Boat, Person, Place
-    
+
 
 class EventType(models.Model):
     name = models.CharField(max_length=100)
-    
+
     class Meta:
         ordering = ['name']
-        
+
     def __unicode__(self):
         return self.name
-    
+
 class EventFlag(models.Model):
     name = models.CharField(max_length=100)
     def __unicode__(self):
         return self.name
-    
+
 class Event(models.Model):
     name = models.CharField(max_length=100)
     type = models.ForeignKey(EventType, related_name="events")
@@ -28,57 +28,64 @@ class Event(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField(blank=True, null=True)
     location = models.ForeignKey(Place, related_name="hosts")
-    
+
     slug = models.SlugField(editable=False)
-    
+
     def __unicode__(self):
         return self.name + ' (%s)' % (self.start.strftime('%Y'))
-        
+
     @models.permalink
     def get_absolute_url(self):
         return ('event_view', [self.slug])
-        
+
     class Meta:
         ordering = ['-start']
-        
+
     def save(self, *args, **kwargs):
-        
+
         self.slug = slugify(self.name + ' ' + str(self.start))
         return super(Event, self).save(*args, **kwargs)
-    
+
+    def get_filterable_classes(self):
+        klasses = ['event', slugify(self.type)]
+        if self.results.count():
+            klasses.append('results')
+            
+        for flag in self.flags.all():
+            klasses.append(slugify(flag.name))
+            klasses.append(slugify(flag.name + str(self.start.year)))
+
+        return ' '.join(klasses)
+
+
 class RaceResult(models.Model):
     event = models.ForeignKey(Event, related_name='results')
     report = models.TextField(blank=True)
     full_results = models.FileField(upload_to='results', blank=True)
-    
+
     def __unicode__(self):
         return str(self.event) + ' Results'
-        
+
 class RaceTrophy(models.Model):
     name = models.CharField(max_length=100)
     boat = models.ForeignKey(Boat, related_name="race_trophies")
     helm = models.ForeignKey(Person, related_name="helming_trophies")
     crew = models.ForeignKey(Person, related_name="crewing_tropies")
-    
+
     raceresult = models.ForeignKey(RaceResult, related_name="trophies")
-    
+
     def __unicode__(self):
         return '%s - %s (%s, %s)' % (self.name, self.boat.name, self.helm.name, self.crew.name)
-    
+
 
 class RacePosition(models.Model):
     position = models.IntegerField()
-    
+
     boat = models.ForeignKey(Boat, related_name="race_positions")
     helm = models.ForeignKey(Person, related_name="helming_positions")
     crew = models.ForeignKey(Person, related_name="crewing_positions")
     points = models.IntegerField(blank=True, null=True)
     raceresult = models.ForeignKey(RaceResult, related_name='positions')
-    
+
     def __unicode__(self):
         return '%s - %s' % (ordinal(self.position), self.boat.name)
-    
-
-    
-    
-    
